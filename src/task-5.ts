33d465fa6c66d11ff246c3e41b816aede585c1ea
Anton1256 <anton.bidenko1@gmail.com>
2025-06-14 16:29:10 +0300

    done

---
+++ src/task-5.ts
@@ -0,0 +1,13 @@
+type stat = "loading" | "success" | "error";
+
+function logStatus(status:stat): void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
